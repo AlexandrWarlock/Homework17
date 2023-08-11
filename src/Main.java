274ef0ad76bfab91fa3130a083f087c67f217a8a
AlexandrWarlock <alexandrpetrov291094@gmail.com>
2023-08-11 13:46:29 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            AuthenticationValidator.checkAuthentication("qeffqgbev1",
+                    "password", "passwword");
+            System.out.println("Аутентификация проейдена успешно");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
